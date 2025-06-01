918c89d2752fb4c206d64726224c33ca7c81f64b
guisaul <guilhermesaul190@gmail.com>
2025-05-31 23:05:07 -0300

    Rodando a animação quando a personagem anda

--- conteúdo/Deadlylight/main.py
+++ conteúdo/Deadlylight/main.py
@@ -5,6 +5,7 @@ from player import Player
 pygame.init()
 
 player = Player()
+
 largura = 1280
 altura = 720
 tela = pygame.display.set_mode((largura, altura), 0)
@@ -18,10 +19,10 @@ while True:
         if event.type == pygame.QUIT:
             exit()
             
+    relogio.tick(30)
     tela.blit(fundo, (0, 0))
-    relogio.tick(60)
     teclas = pygame.key.get_pressed()
     player.draw(tela)
-    player.andar(teclas)
-    
+    player.update(teclas)
+     
     pygame.display.flip()
--- conteúdo/Deadlylight/player.py
+++ conteúdo/Deadlylight/player.py
@@ -6,24 +6,32 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
         self.sprites = []
         self.caminhos = [
-            os.path.join(os.path.dirname(__file__), "data", "images", f"andar_direita1.png")
+            os.path.join(os.path.dirname(__file__), "data", "images", f"andar_direita{i}.png")
+            for i in range (1, 6)
         ]
+        
         for caminho in self.caminhos:
             self.sprites.append(pygame.image.load(caminho))
         self.atual = 0
         self.image = self.sprites[self.atual]
         self.image = pygame.transform.scale(self.image, (64*3, 64*3))
         self.x = 300
-        self.y = 343
+        self.y = 337
         self.rect = self.image.get_rect()
         self.rect.top = self.y
         self.rect.left = self.x
-        self.velocidade = 5
+        self.velocidade = 7
         
-    def andar(self, teclas):
+    def update(self, teclas):
         if teclas[pygame.K_d]:
             self.x += self.velocidade
             self.rect.left = self.x
+            self.atual += 0.2
+            if self.atual >= len(self.sprites):
+                self.atual = 0
+            self.image = self.sprites[int(self.atual)]
+            self.image = pygame.transform.scale(self.image, (64*3, 64*3))
+            
             
         
     def draw(self, tela):
